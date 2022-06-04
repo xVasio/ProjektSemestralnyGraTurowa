2841c1494cbe47d2a1b636195f8800fd173fedfb
xVasio <63720764+xVasio@users.noreply.github.com>
2022-06-04 10:55:03 +0200

    Special Ability and Creature implemented

--- Creature.cpp
+++ Creature.cpp
@@ -5,27 +5,9 @@
 #include <string>
 #include <vector>
 
-enum class CreatureType {
-    Water, Earth, Air, Fire, Ice, Steel
-};
 // make an interaction table for every type, water is less efective against water, fire is more effective against water and so on
 // the table is a 2d array, the first index is the attacker type, the second index is the defender type
 // the value is the effectiveness
-const int interactionTable[6][6] = {
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0}
-};
-
-
-
-class SpecialAbility {
-    /// TODO: add special abilities
-};
-
 
 
 auto getEffectiveness(Creature &attacker, Creature &defender) -> int {
--- Creature.hpp
+++ Creature.hpp
@@ -1,33 +1,88 @@
 //
 // Created by theer on 24.05.2022.
 //
+#pragma once
 
 #include <string>
 #include <vector>
-
-#ifndef PROJEKTSEMESTRALNYGRATUROWA_CREATURE_HPP
-#define PROJEKTSEMESTRALNYGRATUROWA_CREATURE_HPP
+#include <functional>
 
 enum class CreatureType {
     Water, Earth, Air, Fire, Ice, Steel
 };
 
 
-
 class Creature {
+    using SpecialAbility =  std::function<Creature&(Creature&)>;
     std::string Name;
     int power;
     int agility;
     int health;
     int Exp;
     CreatureType type;
-    SpecialAbility& specialAbility;
+    SpecialAbility specialAbility;
+    constexpr inline static int interactionTable[6][6] = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0}
+    };
+public:
+    virtual auto getType() const -> CreatureType = 0;
 
-    Creature(const std::string &name, int power, int agility, int health, int exp, SpecialAbility &specialAbility)
+
+    Creature(const std::string &name, int power, int agility, int health, int exp, SpecialAbility const &specialAbility)
             : Name(name), power(power), agility(agility), health(health), Exp(exp), specialAbility(specialAbility) {}
+
+};
+
+class WaterCreature : Creature {
+public:
+    using Creature::Creature;
+    CreatureType getType() const override {
+        return CreatureType::Water;
+    }
+};
+
+class EarthCreature : Creature {
+public:
+    using Creature::Creature;
+    CreatureType getType() const override {
+        return CreatureType::Earth;
+    }
+};
+
+class AirCreature : Creature {
+public:
+    using Creature::Creature;
+    CreatureType getType() const override {
+        return CreatureType::Air;
+    }
+};
+
+class FireCreature : Creature {
+public:
+    using Creature::Creature;
+    CreatureType getType() const override {
+        return CreatureType::Fire;
+    }
+};
+class IceCreature : Creature {
+public:
+    using Creature::Creature;
+    CreatureType getType() const override {
+        return CreatureType::Ice;
+    }
+};
+class SteelCreature : Creature {
+public:
+    using Creature::Creature;
+    CreatureType getType() const override {
+        return CreatureType::Steel;
+    }
 };
 
 
 
-
-#endif //PROJEKTSEMESTRALNYGRATUROWA_CREATURE_HPP
